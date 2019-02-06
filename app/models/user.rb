46b38b5a4c346e34647f719f73f2037114bb16e4
adudley78 <dudleyadam@gmail.com>
2019-02-06 16:54:05 -0500

    Add username and email uniqueness validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,11 @@ class User < ActiveRecord::Base
   # macro that gives each instance of a User access to password encryption methods via bcrypt
   has_secure_password
 
-  # username and email must be present on a User instance before it can be saved
-  validates :username, presence: true
-  validates :email, presence: true
-  # add validation for proper email structure
+  # unique username and email must be present on a User instance before it can be saved
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
 
-  # def already_a_user?
-  #   User.find_by(username: self.username)
-  #   self.errors.add(:email, "already registered to an existing user.") if flag
-  #   flag
-  # end
-
- #  def slug
- #   username.downcase.gsub(" ","-")
- #  end
- #
- # def self.find_by_slug(slug)
- #   User.all.find{|user| user.slug == slug}
- # end
+  # create unique_user? method with error messages to call on registration
+  # create username_suggester method with unique username suggestions to call on registration
 
 end
